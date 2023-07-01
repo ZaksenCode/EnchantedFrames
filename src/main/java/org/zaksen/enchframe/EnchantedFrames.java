24bfa295005951d3af40086b1d20c33dd51bcec8
Zaksen <maksimsidorkin2006@mail.ru>
2023-07-01 18:49:53 +0300

    0.1 fix

--- src/main/java/org/zaksen/enchframe/EnchantedFrames.java
+++ src/main/java/org/zaksen/enchframe/EnchantedFrames.java
@@ -1,13 +1,16 @@
 package org.zaksen.enchframe;
 
 import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-public class EnchantedFrames implements ModInitializer {
-    /**
-     * Runs the mod initializer.
-     */
+public class EnchantedFrames implements ModInitializer
+{
+    public static final String MOD_ID = "enchframe";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
     @Override
-    public void onInitialize() {
-
+    public void onInitialize()
+    {
+        LOGGER.info("Start initialization...");
     }
 }
--- src/main/java/org/zaksen/enchframe/mixin/ItemFrameMixin.java
+++ src/main/java/org/zaksen/enchframe/mixin/ItemFrameMixin.java
@@ -1,2 +1,41 @@
-package org.zaksen.enchframe.mixin;public class ItemFrameMixin {
+package org.zaksen.enchframe.mixin;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.decoration.ItemFrameEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(ItemFrameEntity.class)
+public abstract class ItemFrameMixin
+{
+    @Shadow private boolean fixed;
+
+    @Shadow protected abstract void dropHeldStack(@Nullable Entity entity, boolean alwaysDrop);
+
+    @Inject(method = "interact", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/decoration/ItemFrameEntity;playSound(Lnet/minecraft/sound/SoundEvent;FF)V", shift = At.Shift.BEFORE), cancellable = true)
+    private void onInteract(PlayerEntity player, Hand hand, CallbackInfoReturnable<ActionResult> info) {
+        if (player.isSneaking())
+        {
+            ItemFrameEntity frame = ((ItemFrameEntity) (Object) this);
+            frame.setInvisible(!frame.isInvisible());
+            this.fixed = !this.fixed;
+
+            info.setReturnValue(ActionResult.SUCCESS);
+        }
+    }
+
+    @Inject(method = "damage", at = @At(value = "HEAD"))
+    private void onDamage(DamageSource source, float amount, CallbackInfoReturnable<Boolean> info) {
+        ItemFrameEntity frame = ((ItemFrameEntity) (Object) this);
+        frame.setInvisible(false);
+        this.fixed = false;
+    }
 }
